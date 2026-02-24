64a1c416fa2d801b363e64aef4e10c3b8c31c559
OzamGameOver <eslamhosny979@gmail.com>
2026-02-24 18:43:05 +0200

    Update detailpage.controller.js

--- src/controllers/detailpage.controller.js
+++ src/controllers/detailpage.controller.js
@@ -1,11 +1,17 @@
 import { extractDetailpage } from '../extractor/extractDetailpage';
 import { axiosInstance } from '../services/axiosInstance';
 import { validationError } from '../utils/errors';
+import blacklist from '../blacklist/blacklist.json' assert { type: 'json' };
 
 import { Redis } from '@upstash/redis';
 
 const detailpageController = async (c) => {
   const id = c.req.param('id');
+  
+   // 🔥 BLACKLIST CHECK
+  if (blacklist.ids.includes(Number(id))) {
+    throw new validationError('This anime is blocked.', 'blacklist');
+  }
 
   const isRedisEnv = Boolean(
     process.env.UPSTASH_REDIS_REST_URL && process.env.UPSTASH_REDIS_REST_TOKEN
