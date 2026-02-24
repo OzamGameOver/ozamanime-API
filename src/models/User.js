fd1a7f333609649a7f55975a7afea42866ccbcdb
OzamGameOver <eslamhosny979@gmail.com>
2026-02-24 05:27:51 +0200

    Update User.js

--- src/models/User.js
+++ src/models/User.js
@@ -1,1 +1,25 @@
+const mongoose = require("mongoose");
 
+const userSchema = new mongoose.Schema({
+  username: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+
+  savedAnime: [
+    {
+      animeId: String,
+      title: String,
+      image: String
+    }
+  ],
+
+  watchlist: [
+    {
+      animeId: String,
+      title: String,
+      image: String
+    }
+  ]
+}, { timestamps: true });
+
+module.exports = mongoose.model("User", userSchema);
