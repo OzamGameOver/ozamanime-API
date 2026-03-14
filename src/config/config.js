82f988c7c135777adbab2d0a6ffb839977379d94
OzamGameOver <eslamhosny979@gmail.com>
2026-03-14 16:01:16 +0200

    Update config.js

--- src/config/config.js
+++ src/config/config.js
@@ -1,10 +1,6 @@
 const config = {
-  baseurl: 'https://hianime.do',
-  baseurl_v2: 'https://kaido.to',
-  providers: 'https://megacloud.club',
-
-  headers: {
-    'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64; rv:122.0) Gecko/20100101 Firefox/122.0',
-  },
+  baseurl: 'https://api.consumet.org/anime/gogoanime/',
+  baseurl_v2: 'https://api.consumet.org/anime/animepahe/',
+  providers: 'https://api.consumet.org/anime/providers/',
 };
 export default config;
